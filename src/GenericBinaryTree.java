d41db0c89897c42b5de2ebc3c5be018943a8c252
Abhinav Bhardwaj <abhinavbhardwaj68@gmail.com>
2024-01-21 01:57:02 +0530

    Method to count total number of nodes present in the Binary Tree and method to construct Binary Tree with level wise input.

--- src/GenericBinaryTree.java
+++ src/GenericBinaryTree.java
@@ -1,3 +1,5 @@
+import java.util.LinkedList;
+import java.util.Queue;
 import java.util.Scanner;
 
 /**
@@ -39,6 +41,54 @@ public class GenericBinaryTree {
         return root;
     }
 
+    public static BinaryTreeNode<Integer> createBinaryTreeLevelWise() {      // input will be inserted in the tree level wise.
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter data for root node: ");
+
+        int data = sc.nextInt();
+
+        if (data == -1) {
+            return null;
+        }
+
+        BinaryTreeNode<Integer> root = new BinaryTreeNode<Integer>(data);
+        Queue<BinaryTreeNode<Integer>> pendingNodes = new LinkedList<BinaryTreeNode<Integer>>();
+        pendingNodes.add(root);
+
+        while (!pendingNodes.isEmpty()) {
+            BinaryTreeNode<Integer> frontNode = pendingNodes.poll();
+
+            System.out.print("Enter left child node of " + frontNode.data);
+            int leftChildNodeData = sc.nextInt();
+
+            if(leftChildNodeData != -1) {
+                BinaryTreeNode<Integer> leftChildNode = new BinaryTreeNode<Integer>(leftChildNodeData);
+                frontNode.leftChild = leftChildNode;
+                pendingNodes.add(leftChildNode);
+            }
+
+            System.out.print("Enter right child node of " + frontNode.data);
+            int rightChildNodeData = sc.nextInt();
+
+            if(rightChildNodeData != -1) {
+                BinaryTreeNode<Integer> rightChildNode = new BinaryTreeNode<Integer>(rightChildNodeData);
+                frontNode.rightChild = rightChildNode;
+                pendingNodes.add(rightChildNode);
+            }
+        }
+
+        return root;
+    }
+
+    public static int totalNumberOfNodes(BinaryTreeNode<Integer> root) {
+        if(root == null) {
+            return 0;
+        }
+
+        return totalNumberOfNodes(root.leftChild) + totalNumberOfNodes(root.rightChild) + 1;    // 1 for root node itself
+    }
+
     public static void viewDetailedBinaryTree(BinaryTreeNode<Integer> root) {
         if (root == null) {
             return;
@@ -64,6 +114,16 @@ public class GenericBinaryTree {
     public static void main(String[] args) {
         BinaryTreeNode<Integer> root = createBinaryTree(true, -1, true);
 
+        System.out.println("Total number of nodes present in this Binary Tree is: " + totalNumberOfNodes(root));
+
+        System.out.print("Detailed view of this Binary Tree is: ");
+        viewDetailedBinaryTree(root);
+
+        // for level wise input
+        root = createBinaryTreeLevelWise();
+        System.out.println("Total number of nodes present in this Binary Tree is: " + totalNumberOfNodes(root));
+
+        System.out.print("Detailed view of this Binary Tree is: ");
         viewDetailedBinaryTree(root);
     }
 }
